8e389ae04a2645ff924240876b384787ef0ee3e6
Jim Shepard <dseisoglou@gmail.com>
2013-06-11 13:33:11 +0300

    Created a rails app and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wofli::Application.config.secret_token = '4c55fae080e3476111a28c5ec72c139fb6f7b2ded222c15e1b0d00776195f135a8e6d40e783bbae2421ee3291840502da331fbc221bd719eec675183aaa2e375'
